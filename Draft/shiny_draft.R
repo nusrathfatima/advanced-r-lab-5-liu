1cec24e44acdd1766477388b3deaaa28b71cc036
Arian Barakat <arianbarakat@c-134-230.eduroam.liu.se>
2016-09-27 15:44:39 +0200

    Adding the drafts

---
+++ Draft/shiny_draft.R
@@ -0,0 +1,102 @@
+library(shiny)
+source("/Users/arianbarakat/Desktop/Linköpings Universitet/Statistics and Data Mining/Semester 1/Advanced Programming in R, 732A94/Labs/Lab5/Draft/api_draft.R")
+library(leaflet)
+library(xtable)
+
+
+ui <- fluidPage(
+  titlePanel("Address finder"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      wellPanel(
+        selectInput(inputId = "inputtype",
+                    label = "Select input type",
+                    choices = c("Address","Coordinates"),
+                    selected = "Address"),
+        br(),
+        uiOutput("ui"),
+        br(),
+        br(),
+        selectInput(inputId = "maptype",
+                  label = "Choose map type",
+                  c("Standard" = "OpenStreetMap.Mapnik",
+                    "Satellite" = "Esri.WorldImagery")),
+      br()
+      )),
+    
+    mainPanel(
+      leafletOutput("mymap", height = 700),
+      tableOutput("mytable")
+    )
+  ))
+
+
+
+
+server <- function(input,output){
+  
+  output$ui <- renderUI({
+    switch (input$inputtype,
+      "Address" = textInput(inputId = "Address",
+                            label = "Adress",
+                            value = "Mäster Mattias väg, Linköping, Sweden"),
+      "Coordinates" = 
+        textInput(inputId = "long",
+                                  label = "Longitud and Latitud",
+                                  value = "58.39701 15.57415")
+    )
+  })
+
+  ## Plot
+  
+  plot_out <- reactive({ 
+      if(input$inputtype == "Address"){
+      points <- coord_lookup(address = input$Address)[c("lat","lng")]
+      
+      id <- coord_lookup(address = input$Address)["Full_address"]
+      
+      leaflet() %>%
+        addProviderTiles(input$maptype,
+                         options = providerTileOptions(noWrap = TRUE)
+        ) %>%
+        addMarkers(data = points, icon = id )
+      
+    } else {
+      points <- address_lookup(latlong = input$long)[c("lat","lng")]
+      
+      id <- address_lookup(latlong = input$long)["Full_address"]
+      
+      leaflet() %>%
+        addProviderTiles(input$maptype,
+                         options = providerTileOptions(noWrap = TRUE)
+        ) %>%
+        addMarkers(data = points, icon = id )
+      
+    }
+  })
+  
+  output$mymap <- renderLeaflet({
+    plot_out()
+  })
+  
+  # Table with Full address name and coord
+  
+  table_out <- reactive({
+    if(input$inputtype == "Address"){
+      x <- coord_lookup(address = input$Address)
+      colnames(x) <- c("Full Address Name","Latitud","Longitud")
+      x
+    } else {
+      x <- address_lookup(latlong = input$long)
+      colnames(x) <- c("Full Address Name","Latitud","Longitud")
+      x
+    }
+  })
+  
+  output$mytable <- renderTable(table_out(), hover = TRUE, bordered = TRUE, digits = 5)
+    
+}
+
+
+shinyApp(ui = ui, server = server)
